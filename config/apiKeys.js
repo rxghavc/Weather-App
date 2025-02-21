1b64be1d8319f2b7e23adc33f781a09f822f90bd
rxghavc <r.commandur@gmail.com>
2025-02-21 22:15:42 +0000

    Started function to fetch weather data, waiting for API key to work

--- config/apiKeys.js
+++ config/apiKeys.js
@@ -1,3 +1,2 @@
 const apiKey = 'c50064f357dbfd34d754b2f50be4ab5c';
-const urlUsed = "https://api.openweathermap.org/data/2.5/weather?q=germany&appid=c50064f357dbfd34d754b2f50be4ab5c&units=metric";
 export default apiKey;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import apiKey from "../config/apiKeys"
+import apiURL from "../config/apiURL"
+
+async function getWeatherData() {
+    const response = await fetch(`${apiURL}&appid=${apiKey}`);
+    var data = await respone.json();
+    console.log(data);
+}
+
+getWeatherData();
